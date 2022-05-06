52306bbb3785b39218cd3a3fc565cf12ddc169df
Hannah <hannahxchapman@yahoo.com>
2022-05-06 14:11:53 -0700

    operator function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,20 @@ function multiply (initialInput, nextInput) {
 function divide (initialInput, nextInput) {
     return initialInput / nextInput;
 };
+
+function operate (operator, initialInput, nextInput) {
+    switch(operator) {
+        case 'add' :
+            add(initialInput, nextInput)
+            break;
+        case 'substract' :
+            substract(initialInput, nextInput)
+            break;
+        case 'multiply' :
+            multiply(initialInput, nextInput)
+            break;
+        case 'divide' :
+            divide(initialInput, nextInput)
+            break;
+    }
+};
