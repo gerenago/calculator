2dc7b8c20fbe8057ed416832d1ef491c36933a9f
Hannah <hannahxchapman@yahoo.com>
2022-05-06 13:55:52 -0700

    function operators

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add (initialInput, nextInput) {
+    return initialInput + nextInput;
+};
+
+function subtract (initialInput, nextInput) {
+    return initialInput - nextInput;
+};
+
+function multiply (initialInput, nextInput) {
+    return initialInput * nextInput;
+};
+
+function divide (initialInput, nextInput) {
+    return initialInput / nextInput;
+};
