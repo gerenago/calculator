9cce9e30aa2ad1271b6b69de0e986ff7cf0dbe52
Hannah <hannahxchapman@yahoo.com>
2022-05-09 14:31:18 -0700

    change events to functions

--- javascript.js
+++ javascript.js
@@ -1,64 +1,63 @@
-//everything is a function!
 
-function start () {
-    let displayValue = '';
-    let initialInput = '';
-    let nextInput = '';
-    let calcoperator = '';
-    let displaySolution = '';
+let displayValue = '';
+let initialInput = '';
+let nextInput = '';
+let calcOperator = '';
+let displaySolution = '';
 
-    function add (initialInput, nextInput) {
-        return initialInput + nextInput;
-    };
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const display = document.querySelector('#display');
 
-    function subtract (initialInput, nextInput) {
-        return initialInput - nextInput;
-    };
 
-    function multiply (initialInput, nextInput) {
-        return initialInput * nextInput;
-    };
+operators.forEach((operator) => {
+    operator.addEventListener('click', getOperator)
+});
 
-    function divide (initialInput, nextInput) {
-        return initialInput / nextInput;
-    };
 
-    function operate (calcoperator, initialInput, nextInput) {
-        switch(calcoperator) {
-            case 'add' :
-                return add(initialInput, nextInput)
-            case 'substract' :
-                return subtract(initialInput, nextInput)
-            case 'multiply' :
-                return multiply(initialInput, nextInput)
-            case 'divide' :
-                return divide(initialInput, nextInput)
+numbers.forEach((number) => {
+    number.addEventListener('click', displayNumbers)
+});
+
+function getOperator (operator) {
+    calcOperator = operator.value;
+    return calcOperator;
+}
+
+function displayNumbers () {
+    if (calcOperator = '') {
+        initialInput += `${number.value}`;
+        display.textContent = `${initialInput}`;
+        } else {
+            nextInput += `${number.value}`;
+            display.textContent = `${nextInput}`;
         }
-    };
-    //listen for each number being pressed
-    const numbers = document.querySelectorAll('.number');
-    numbers.forEach((number) => {
-        number.addEventListener('click', () => {
-            //when clicked, add value to initalInput string
-            if (calcoperator = '') {
-            initialInput += `${number.value}`;
-            const display = document.querySelector('#display');
-            display.textContent = `${initialInput}`;
-            } else {
-                nextInput += `${number.value}`;
-                const display = document.querySelector('#display');
-                display.textContent = `${nextInput}`;
-            }
-        })
-    });
-    //listen for operator being pressed
-    const operators = document.querySelectorAll('.operator');
-    operators.forEach((operator) => {
-        operator.addEventListener('click', () => {
-            //when clicked, store operator value
-            calcoperator = operator.value;
-            return calcoperator;
-        })
-    });
+}
+
+function add (initialInput, nextInput) {
+    return initialInput + nextInput;
 };
-start()
+
+function subtract (initialInput, nextInput) {
+    return initialInput - nextInput;
+};
+
+function multiply (initialInput, nextInput) {
+    return initialInput * nextInput;
+};
+
+function divide (initialInput, nextInput) {
+    return initialInput / nextInput;
+};
+function operate (calcOperator, initialInput, nextInput) {
+    switch(calcOperator) {
+        case 'add' :
+            return add(initialInput, nextInput)
+        case 'substract' :
+            return subtract(initialInput, nextInput)
+        case 'multiply' :
+            return multiply(initialInput, nextInput)
+        case 'divide' :
+            return divide(initialInput, nextInput)
+    }
+};
