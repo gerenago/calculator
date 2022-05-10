91fe575a3569560b3922af08d9dd7b98ba08191e
Hannah <hannahxchapman@yahoo.com>
2022-05-09 17:02:37 -0700

    can't get anything to work

--- javascript.js
+++ javascript.js
@@ -1,39 +1,89 @@
-
-let displayValue = '';
+//We need initial input
+//In order to grab initial input, we need to check if buttons are pressed
+//if a button is pressed, it needs to be part of the initialinput value
+//how do you check if a button is pressed?
+//how do you check WHICH button is pressed?
 let initialInput = '';
 let nextInput = '';
-let calcOperator = '';
-let displaySolution = '';
 
-const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operator');
+const zero = document.querySelector('.zero');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const adding = document.querySelector('.add');
+const subtracting = document.querySelector('.subtract');
+const multiplying = document.querySelector('.multiply');
+const dividing = document.querySelector('.divide');
 const display = document.querySelector('#display');
 
-
-operators.forEach((operator) => {
-    operator.addEventListener('click', getOperator)
-});
-
-
-numbers.forEach((number) => {
-    number.addEventListener('click', displayNumbers)
-});
-
-function getOperator (operator) {
-    calcOperator = operator.value;
-    return calcOperator;
+zero.onclick = function onZero() {
+    initialInput += '0'
+    display.textContent = `${initialInput}`;
+    
 }
 
-function displayNumbers () {
-    if (calcOperator = '') {
-        initialInput += `${number.value}`;
+one.onclick = function onOne() {
+        initialInput += '1'
         display.textContent = `${initialInput}`;
-        } else {
-            nextInput += `${number.value}`;
-            display.textContent = `${nextInput}`;
-        }
+        
 }
 
+two.onclick = function onTwo() {
+        initialInput += '2'
+        display.textContent = `${initialInput}`;
+        
+}
+
+three.onclick = function onThree() {
+        initialInput += '3'
+        display.textContent = `${initialInput}`;
+        
+}
+
+four.onclick = function onFour() {
+        initialInput += '4'
+        display.textContent = `${initialInput}`;
+        
+}
+
+five.onclick = function onFive() {
+        initialInput += '5'
+        display.textContent = `${initialInput}`;
+        
+}
+
+six.onclick = function onSix() {
+        initialInput += '6'
+        display.textContent = `${initialInput}`;
+        
+}
+
+seven.onclick = function onSeven() {
+        initialInput += '7'
+        display.textContent = `${initialInput}`;
+        
+}
+
+eight.onclick = function onEight() {
+        initialInput += '8'
+        display.textContent = `${initialInput}`;
+}
+
+nine.onclick = function onNine() {
+        initialInput += '9'
+        display.textContent = `${initialInput}`;
+}
+
+
+
+
+//operations
 function add (initialInput, nextInput) {
     return initialInput + nextInput;
 };
