7a40d36bc3ac1100adf4dc3c6f88a4fcd35c53c5
Hannah <hannahxchapman@yahoo.com>
2022-05-19 21:02:11 -0700

    WE DID IT

--- javascript.js
+++ javascript.js
@@ -4,49 +4,55 @@
 //how do you check if a button is pressed?
 //how do you check WHICH button is pressed?
 let initialInput = '';
-let nextInput = '';
 let currentInput = '';
+let calcOperator = '';
 
 const display = document.querySelector('#display');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
 
 //when operator is clicked, pass number to oldnum and save operator
+numbers.forEach(number => number.addEventListener('click', numberClicked));
+
+function numberClicked () {
+    currentInput += this.value;
+    display.textContent = `${currentInput}`;
+};
+
+operators.forEach(operator => operator.addEventListener('click', moveNum));
 function moveNum () {
     initialInput = currentInput;
     currentInput = '';
+    calcOperator = this.value;
 };
 
-$("numbers").click(function () {
-    currentInput += $(this).val();
-    display.textContent = `${currentInput}`;
-})
+equals.addEventListener('click', operate);
 
 //operations
-function add (initialInput, nextInput) {
-    return initialInput + nextInput;
-};
 
-function subtract (initialInput, nextInput) {
-    return initialInput - nextInput;
-};
-
-function multiply (initialInput, nextInput) {
-    return initialInput * nextInput;
-};
-
-function divide (initialInput, nextInput) {
-    return initialInput / nextInput;
-};
-function operate (calcOperator, initialInput, nextInput) {
+function operate () {
     switch(calcOperator) {
-        case 'add' :
-            return add(initialInput, nextInput)
-        case 'substract' :
-            return subtract(initialInput, nextInput)
-        case 'multiply' :
-            return multiply(initialInput, nextInput)
-        case 'divide' :
-            return divide(initialInput, nextInput)
+        case "add" :
+            initialInput = parseInt(initialInput);
+            currentInput = parseInt(currentInput);
+            currentInput = initialInput + currentInput;
+            break;
+        case "subtract" :
+            initialInput = parseInt(initialInput);
+            currentInput = parseInt(currentInput);
+            currentInput = initialInput - currentInput;
+            break;
+        case "multiply" :
+            initialInput = parseInt(initialInput);
+            currentInput = parseInt(currentInput);
+            currentInput = initialInput * currentInput;
+            break;
+        case "divide" :
+            initialInput = parseInt(initialInput);
+            currentInput = parseInt(currentInput);
+            currentInput = initialInput / currentInput;
+            break;
     }
+    display.textContent = `${currentInput}`;
 };
