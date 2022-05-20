7ee994e4dedfd85ce7cc662ad497827d4f8f44c1
Hannah <hannahxchapman@yahoo.com>
2022-05-19 17:04:45 -0700

    figuring out calculations

--- javascript.js
+++ javascript.js
@@ -5,83 +5,22 @@
 //how do you check WHICH button is pressed?
 let initialInput = '';
 let nextInput = '';
+let currentInput = '';
 
-const zero = document.querySelector('.zero');
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
-const adding = document.querySelector('.add');
-const subtracting = document.querySelector('.subtract');
-const multiplying = document.querySelector('.multiply');
-const dividing = document.querySelector('.divide');
 const display = document.querySelector('#display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
 
-zero.onclick = function onZero() {
-    initialInput += '0'
-    display.textContent = `${initialInput}`;
-    
-}
+//when operator is clicked, pass number to oldnum and save operator
+function moveNum () {
+    initialInput = currentInput;
+    currentInput = '';
+};
 
-one.onclick = function onOne() {
-        initialInput += '1'
-        display.textContent = `${initialInput}`;
-        
-}
-
-two.onclick = function onTwo() {
-        initialInput += '2'
-        display.textContent = `${initialInput}`;
-        
-}
-
-three.onclick = function onThree() {
-        initialInput += '3'
-        display.textContent = `${initialInput}`;
-        
-}
-
-four.onclick = function onFour() {
-        initialInput += '4'
-        display.textContent = `${initialInput}`;
-        
-}
-
-five.onclick = function onFive() {
-        initialInput += '5'
-        display.textContent = `${initialInput}`;
-        
-}
-
-six.onclick = function onSix() {
-        initialInput += '6'
-        display.textContent = `${initialInput}`;
-        
-}
-
-seven.onclick = function onSeven() {
-        initialInput += '7'
-        display.textContent = `${initialInput}`;
-        
-}
-
-eight.onclick = function onEight() {
-        initialInput += '8'
-        display.textContent = `${initialInput}`;
-}
-
-nine.onclick = function onNine() {
-        initialInput += '9'
-        display.textContent = `${initialInput}`;
-}
-
-
-
+$("numbers").click(function () {
+    currentInput += $(this).val();
+    display.textContent = `${currentInput}`;
+})
 
 //operations
 function add (initialInput, nextInput) {
